b8365707673d00bdc494acd8e4e6c6142f8c0b5a
MarcioDzn <marcio.roberto0739@gmail.com>
2023-07-25 19:34:07 -0300

    :sparkles: feat: Add hover effect on squares

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,17 @@
 const canvas = document.querySelector('.container');
 
+const GRAY = `rgb(127.5, 127.5, 127.5)`;
+const BLACK = `rgb(0, 0, 0)`;
+
 const GRID_SIZE = 16; // Npx x Npx
 const SQUARE_SIZE = 100 / GRID_SIZE;
 
+let isPainting = false;
+
 createSquareDivs();
+
+const squareArray = canvas.querySelectorAll('.square');
+squareArray.forEach(paintSquare);
 
 function createSquareDivs(){
     for (let i = 0; i < GRID_SIZE*GRID_SIZE; i++){
@@ -14,4 +22,38 @@ function createSquareDivs(){
 
         canvas.appendChild(square);
     }
+}
+
+function paintSquare(square){
+    // Prevents continuing to paint even after removing the mouse from the canvas and returning
+    window.addEventListener('mousemove', (e) => {
+        if (e.target.parentElement != canvas) isPainting = false;
+    })
+
+    square.addEventListener('dragstart', (e) => {e.preventDefault();}); // Prevents drag and drop action
+
+    square.addEventListener('mousedown', (e) => {
+        isPainting = true;
+        paintSquareGray(e);
+    });
+
+    square.addEventListener('mouseup', (e) => {
+        paintSquareBlack(e)
+        isPainting = false;
+    });
+
+    square.addEventListener('mouseenter', paintSquareGray);
+    square.addEventListener('mouseleave', paintSquareBlack);
+}
+
+function paintSquareGray(e){
+    if (!isPainting || getComputedStyle(e.target).backgroundColor == BLACK) return;
+    e.target.style.backgroundColor = GRAY;
+
+}
+
+function paintSquareBlack(e){
+    if (!isPainting) return;
+    e.target.style.backgroundColor = BLACK;
+
 }
