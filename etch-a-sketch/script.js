bc5ecd7f0950aa2bf7d81382b4e44eef8f9cb3af
MarcioDzn <marcio.roberto0739@gmail.com>
2023-07-26 15:03:09 -0300

    :sparkles: feat: Add canvas dimension customization

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,23 +1,29 @@
-const canvas = document.querySelector('.container');
+const canvas = document.querySelector('.canvas');
+const btnDimensions = document.querySelector('.btn-dimensions');
+const canvasDimensionsValue = document.querySelector('.canvas-dimensions-value');
 
 const GRAY = `rgb(127.5, 127.5, 127.5)`;
 const BLACK = `rgb(0, 0, 0)`;
 
-const GRID_SIZE = 16; // Npx x Npx
-const SQUARE_SIZE = 100 / GRID_SIZE;
+let gridSize = 16; // Npx x Npx
+let squareSize = 100 / gridSize;
 
 let isPainting = false;
 
-createSquareDivs();
-
-const squareArray = canvas.querySelectorAll('.square');
-squareArray.forEach(paintSquare);
+let squareArray = [];
+// SetTimeout to avoid html block rendering while prompt is active
+setTimeout(() => {
+    changeCanvasDimensions();
+}, 20);
+btnDimensions.addEventListener('click', changeCanvasDimensions);
 
 function createSquareDivs(){
-    for (let i = 0; i < GRID_SIZE*GRID_SIZE; i++){
+    canvas.innerHTML = '';
+    squareArray.length = 0;
+    for (let i = 0; i < gridSize*gridSize; i++){
         const square = document.createElement('div');
-        square.style.width = `${SQUARE_SIZE}%`;
-        square.style.height = `${SQUARE_SIZE}%`;
+        square.style.width = `${squareSize}%`;
+        square.style.height = `${squareSize}%`;
         square.setAttribute('class', 'square');
 
         canvas.appendChild(square);
@@ -29,19 +35,15 @@ function paintSquare(square){
     window.addEventListener('mousemove', (e) => {
         if (e.target.parentElement != canvas) isPainting = false;
     })
-
     square.addEventListener('dragstart', (e) => {e.preventDefault();}); // Prevents drag and drop action
-
     square.addEventListener('mousedown', (e) => {
         isPainting = true;
         paintSquareGray(e);
     });
-
     square.addEventListener('mouseup', (e) => {
         paintSquareBlack(e)
         isPainting = false;
     });
-
     square.addEventListener('mouseenter', paintSquareGray);
     square.addEventListener('mouseleave', paintSquareBlack);
 }
@@ -55,5 +57,24 @@ function paintSquareGray(e){
 function paintSquareBlack(e){
     if (!isPainting) return;
     e.target.style.backgroundColor = BLACK;
+}
 
+function changeCanvasDimensions() {
+    let dimension = prompt("Please enter canvas dimensions:\n(16 < n < 64)");
+
+    while (dimension !== null && (isNaN(dimension) || dimension < 16 || dimension > 64 || dimension.trim() === '')) {
+        if (dimension === null) break; // The user clicked Cancel, the loop stops 
+        dimension = prompt("Enter valid dimensions!\n(16 < n < 64)");
+    }
+
+    if (dimension !== null) {
+        gridSize = parseInt(dimension);
+        squareSize = 100 / gridSize;
+    }
+
+    createSquareDivs();
+    
+    squareArray = canvas.querySelectorAll('.square');
+    squareArray.forEach(paintSquare);
+    canvasDimensionsValue.textContent = `Canvas Dimensions: ${gridSize}x${gridSize}.`
 }
