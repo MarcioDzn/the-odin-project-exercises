1fad8dc17d70bfe6841ba63d9fe2543a2878056e
MarcioDzn <marcio.roberto0739@gmail.com>
2023-07-29 12:50:55 -0300

    :sparkles: feat: Adapt the logic to the GUI
    Removes the logic that makes it possible to play through the DevTools console

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,28 +1,27 @@
 const gameChoices = ['rock', 'paper', 'scissors'];
-let roundsAmount = 5;
+
+let playerScore = 0;
+let computerScore = 0;
+
+const choicesBtn = document.querySelectorAll('.player-choice');
+const matchResult = document.querySelector('.match-result');
+const playerMoveDiv = document.querySelector('.player-move');
+const computerMoveDiv = document.querySelector('.computer-move');
+const playerScoreBox = document.querySelector('.player-score span');
+const computerScoreBox = document.querySelector('.computer-score span');
+const gameWinnerBox = document.querySelector('.game-winner');
+
+choicesBtn.forEach(choice => {
+    choice.addEventListener('click', game);
+})
 
 function getComputerChoice(){
     return gameChoices[Math.floor(Math.random() * gameChoices.length)];
 }
 
-function formatString(string){
-    if (string !== null)
-        return string.trim().toLowerCase();
-}
-
-function getPlayerChoice(){
-    let playerChoice = formatString(prompt('Make your move! [Rock, Paper, Scissors]'));
-    while (!gameChoices.includes(playerChoice) || playerChoice === null){
-        playerChoice = formatString(prompt('Invalid move!\nMake your move! [Rock, Paper, Scissors]'));
-    }
-
-    return playerChoice;
-}
-
 function checkFinalWinner(playerScore, computerScore){
     if (playerScore > computerScore) return 'Player Wins!';
     else if (playerScore < computerScore) return 'Computer Wins!';
-    return 'Draw!';
 }
 
 function checkRoundWinner(playerSelection, computerSelection){
@@ -51,25 +50,37 @@ function playRound(playerSelection, computerSelection){
     return `You Lose! '${playerSelection}' loses to '${computerSelection}'.`;
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < roundsAmount; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-
-        const roundWinner = checkRoundWinner(playerSelection, computerSelection);
-        if (roundWinner == 1) playerScore++;
-        else if (roundWinner == -1) computerScore++;
-
-        console.log(`\nRound ${i+1}`);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player Score: ${playerScore} | Computer score: ${computerScore}`);
-    }
-
-    console.log(`========= ${checkFinalWinner(playerScore, computerScore)} =========`);
+function setWinnerBox(active){
+    if (active) gameWinnerBox.style.visibility = 'visible';
+    else gameWinnerBox.style.visibility = 'hidden';
 }
 
-// game();
+function game(e){
+    let playerSelection = '';
+    if (e.target.id == 'rock-btn') playerSelection = 'rock';
+    else if (e.target.id == 'paper-btn') playerSelection = 'paper';
+    else if (e.target.id == 'scissors-btn') playerSelection = 'scissors';
+
+    setWinnerBox(false);
+    const computerSelection = getComputerChoice();
+
+    const roundWinner = checkRoundWinner(playerSelection, computerSelection);
+    if (roundWinner == 1) playerScore++;
+    else if (roundWinner == -1) computerScore++;
+
+    playerMoveDiv.textContent = playerSelection;
+    computerMoveDiv.textContent = computerSelection;
+
+    matchResult.textContent = playRound(playerSelection, computerSelection);
+    playerScoreBox.textContent = playerScore;
+    computerScoreBox.textContent = computerScore;
+    
+    if (playerScore == 5 || computerScore == 5){
+        setWinnerBox(true);
+        gameWinnerBox.textContent = `========= ${checkFinalWinner(playerScore, computerScore)} =========`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+}
+
 
